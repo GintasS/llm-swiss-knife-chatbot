a434272c50dbd4d8b068146c129462feaaafbca8
GintasS <svedgintas@gmail.com>
2024-10-19 00:20:19 -0700

    Added constants.

---
+++ app/core/constants.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class OPENAI:
+  API_KEY = ""
+  MODEL = "gpt-4o"
+
+
+class EMAIL_FUNCTION:
+  EMAIL_ADDRESS=""
+  EMAIL_PASSWORD=""
+
+class CURRENCY_EXCHANGE_FUNCTION:
+  API_KEY=""
+
